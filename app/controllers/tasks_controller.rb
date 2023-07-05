ecb58f5a6e58dee6816d73e9a2854641f1d414e7
Christopher Ramirez Rodriguez <chrisram415@gmail.com>
2023-07-05 14:25:18 +0900

    implemented edit and delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,7 +23,36 @@ class TasksController < ApplicationController
     # task GET    /tasks/:id(.:format)                   tasks#show
     redirect_to task_path(@task)
   end
+
+  # edit_task GET    /tasks/:id/edit(.:format)              tasks#edit
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  # PATCH  /tasks/:id(.:format)                   tasks#update
+  # once we click on update on the edit page we need to trigger this method
+  def update
+    # raise
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  # DELETE /tasks/:id(.:format)                   tasks#destroy
+  def destroy
+    # find the id of the instance w/ params id (we found this by using raise)
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
 end
+
+#  we use link_to helper to connect pages
+# Add _path to the route prefix (left column)
+# start w/ pe tags then link_to "text" or instance info we want as the text, pathway
+# example:
+#  <li><%= link_to restaurant.name, restaurant_path(restaurant) %></li>
+# <%= link_to(title, path) %>
 
 private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,38 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # creates all 7 CRUD actions,
+  # example if this was a restuarant class it would be:
+
+  # # Read all
+  # get    "restaurants",          to: "restaurants#index"
+  # # Create
+  # get    "restaurants/new",      to: "restaurants#new",  as: :new_restaurant
+  # post   "restaurants",          to: "restaurants#create"
+  # # Read one - The `show` route needs to be *after* `new` route.
+  # get    "restaurants/:id",      to: "restaurants#show", as: :restaurant
+  # # Update
+  # get    "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch  "restaurants/:id",      to: "restaurants#update"
+  # # Delete
+  # delete "restaurants/:id",      to: "restaurants#destroy"
+
   resources :tasks
 end
+
+# But we don’t have to write them all!
+# We can generate the 7 routes shown before with just one line:
+# resources :nameofclass
+
+# example:
+# # config/routes.rb
+# Rails.application.routes.draw do
+#   resources :restaurants
+# end
+
+# Subset of the 7 routes aka we can emphasize which routes we want w/:
+# # config/routes.rb
+# Rails.application.routes.draw do
+#   resources :restaurants, only: [:create, :index, :destroy]
+# end
